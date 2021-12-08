8c4243e84724951cd3d8b219d4803a4393cb1667
Büşra Elcir Tanır <92623303+Busratanir@users.noreply.github.com>
2021-12-08 22:38:17 +0300

    Add files via upload

--- hashcoding/hashcoding/Form1.Designer.cs
+++ hashcoding/hashcoding/Form1.Designer.cs
@@ -170,7 +170,7 @@ namespace hashcoding
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(54, 6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(86, 80);
+            this.pictureBox1.Size = new System.Drawing.Size(85, 85);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 10;
             this.pictureBox1.TabStop = false;
@@ -179,7 +179,7 @@ namespace hashcoding
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(619, 480);
+            this.ClientSize = new System.Drawing.Size(631, 480);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
